d7b883debbb0e14f4bbefcd0359733137bf191b6
xhad <chyad@hotmail.com>
2017-09-17 13:02:18 +0900

    basics

---
+++ basics/type-inference.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	b := false // change me!
+	i := 42
+	s := "chad"
+	g := 0.877 + 0.5i
+
+	fmt.Printf("b is of type %T\n", b)
+	fmt.Printf("i is of type %T\n", i)
+	fmt.Printf("s is of type %T\n", s)
+	fmt.Printf("g is of type %T\n", g)
+}
